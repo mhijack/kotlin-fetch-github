40bbfe177fa6e2d36957536b58b06ae05954602a
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-01-18 23:09:15 -0800

    initial setup

--- app/src/main/java/com/example/fetchgithub/MainActivity.kt
+++ app/src/main/java/com/example/fetchgithub/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+
+        var a: String
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+                    .setAction("Action", null).show()
         }
     }
 
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
+        return when(item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
